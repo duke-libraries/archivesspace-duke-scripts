8bc741e9f1d6a1065884931a978005b4575a4090
Noah Huffman <noah.huffman@gmail.com>
2016-06-23 12:21:09 -0400

    modified sql

--- accession_report_simple.sql
+++ accession_report_simple.sql
@@ -1,6 +1,22 @@
-select identifier, title, accession_date, extent.number, enumeration_value.value, extent.extent_type_id, extent.container_summary, content_description, access_restrictions
-from accession
-left join extent on accession.id = extent.accession_id
-left join enumeration_value on extent_type_id = enumeration_value.id
-where accession.accession_date >= '20150701' #change dates as needed
-and accession.accession_date <= '20160317'
+SELECT 
+    accession.identifier,
+    accession.title,
+    accession_date,
+    extent.number as 'extent_number',
+    enumeration_value.value AS 'extent_unit',
+    extent.container_summary,
+    accession.content_description,
+    accession.access_restrictions_note,
+    accession.use_restrictions_note,
+    accession.acquisition_type_id,
+    accession.created_by
+FROM
+    accession
+        LEFT JOIN
+    extent on accession.id = extent.accession_id
+        LEFT JOIN
+    enumeration_value ON extent.extent_type_id = enumeration_value.id
+WHERE
+    accession.accession_date >= '20150701'
+        AND accession.accession_date <= '20160631'
+        AND repo_id = 2
