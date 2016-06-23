9f71321b954eb5474f6e5e05b2f6d0d10dc8addd
Noah Huffman <noah.huffman@gmail.com>
2016-06-23 16:50:49 -0400

    updated sql, again

--- accession_report_simple.sql
+++ accession_report_simple.sql
@@ -2,21 +2,32 @@ SELECT
     accession.identifier,
     accession.title,
     accession_date,
-    extent.number as 'extent_number',
+    extent.number AS 'extent_number',
     enumeration_value.value AS 'extent_unit',
     extent.container_summary,
     accession.content_description,
     accession.access_restrictions_note,
     accession.use_restrictions_note,
     accession.acquisition_type_id,
-    accession.created_by
+    accession.created_by,
+    name_corporate_entity.sort_name AS 'research_center',
+    user_defined.real_1 as 'appraisal_value',
+    (case when user_defined.boolean_2 <> 0 then "yes" else "no" end) as 'ready_for_ts?',
+    (case when user_defined.boolean_1 <> 0 then "yes" else "no" end) as 'electronic media',
+    user_defined.integer_1 as 'aleph_order_number'
 FROM
     accession
+		left join
+	user_defined on accession.id = user_defined.accession_id
         LEFT JOIN
-    extent on accession.id = extent.accession_id
+    linked_agents_rlshp ON accession.id = linked_agents_rlshp.accession_id
+        LEFT JOIN
+    name_corporate_entity ON linked_agents_rlshp.agent_corporate_entity_id = name_corporate_entity.agent_corporate_entity_id
+        LEFT JOIN
+    extent ON accession.id = extent.accession_id
         LEFT JOIN
     enumeration_value ON extent.extent_type_id = enumeration_value.id
 WHERE
     accession.accession_date >= '20150701'
         AND accession.accession_date <= '20160631'
-        AND repo_id = 2
+        AND repo_id = 2;
