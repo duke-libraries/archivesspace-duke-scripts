729e626d226a37d963ebf563089b3649ef629930
Noah Huffman <noah.huffman@gmail.com>
2016-06-23 12:25:27 -0400

    reformatted SQL

--- accession_extent_report.sql
+++ accession_extent_report.sql
@@ -1,10 +1,21 @@
-select value as 'Accession type', COUNT(*) as 'Total accessions measured in linear feet', ROUND(SUM(extent.number), 2) as 'Linear feet'
-from accession
-left join extent on accession.id = extent.accession_id
-left join enumeration_value on acquisition_type_id = enumeration_value.id 
-where (extent_type_id IN (select id from enumeration_value
-where LOWER(value) like '%linear%'))
-and repo_id = 2 #hardcoded value for now
-and accession.accession_date >= '20151001' #change dates as needed
-and accession.accession_date <= '20151231'
-group by acquisition_type_id;
+SELECT 
+    value AS 'Accession type',
+    COUNT(*) AS 'Total accessions measured in linear feet',
+    ROUND(SUM(extent.number), 2) AS 'Linear feet'
+FROM
+    accession
+        LEFT JOIN
+    extent ON accession.id = extent.accession_id
+        LEFT JOIN
+    enumeration_value ON acquisition_type_id = enumeration_value.id
+WHERE
+    (extent_type_id IN (SELECT 
+            id
+        FROM
+            enumeration_value
+        WHERE
+            LOWER(value) LIKE '%linear%'))
+        AND repo_id = 2
+        AND accession.accession_date >= '20151001'
+        AND accession.accession_date <= '20151231'
+GROUP BY acquisition_type_id;
