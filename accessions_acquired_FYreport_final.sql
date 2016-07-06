7e192f53654d7719101f238cb520af4c1bab35a3
Noah Huffman <noah.huffman@gmail.com>
2016-07-06 09:54:22 -0400

    prettified SQL

--- accessions_acquired_FYreport_final.sql
+++ accessions_acquired_FYreport_final.sql
@@ -1,5 +1,5 @@
 SELECT 
-	accession.identifier,
+    accession.identifier,
     accession.title,
     accession.accession_date,
     extent.number,
@@ -11,28 +11,33 @@ SELECT
     accession.content_description,
     accession.access_restrictions_note,
     accession.use_restrictions_note,
-    user_defined.real_1 as 'appraisal_value',
-    (case when user_defined.boolean_2 <> 0 then "yes" else "no" end) as 'ready_for_ts?',
-    (case when user_defined.boolean_1 <> 0 then "yes" else "no" end) as 'electronic media',
-    user_defined.integer_1 as 'aleph_order_number',
+    user_defined.real_1 AS 'appraisal_value',
+    (CASE
+        WHEN user_defined.boolean_2 <> 0 THEN 'yes'
+        ELSE 'no'
+    END) AS 'ready_for_ts?',
+    (CASE
+        WHEN user_defined.boolean_1 <> 0 THEN 'yes'
+        ELSE 'no'
+    END) AS 'electronic media',
+    user_defined.integer_1 AS 'aleph_order_number',
     accession.created_by
 FROM
     accession
-        left JOIN
+        LEFT JOIN
     user_defined ON accession.id = user_defined.accession_id
-		left JOIN
+        LEFT JOIN
     enumeration_value ev2 ON user_defined.enum_2_id = ev2.id
-		left JOIN
-	enumeration_value ev3 ON accession.acquisition_type_id = ev3.id
-		left JOIN
-	enumeration_value ev1 ON user_defined.enum_1_id = ev1.id
-        left JOIN
+        LEFT JOIN
+    enumeration_value ev3 ON accession.acquisition_type_id = ev3.id
+        LEFT JOIN
+    enumeration_value ev1 ON user_defined.enum_1_id = ev1.id
+        LEFT JOIN
     extent ON accession.id = extent.accession_id
-		left join
-	enumeration_value ev4 on extent.extent_type_id = ev4.id
+        LEFT JOIN
+    enumeration_value ev4 ON extent.extent_type_id = ev4.id
 WHERE
     accession.accession_date >= '20150701'
         AND accession.accession_date <= '20160631'
         AND repo_id = 2
-order by
-	accession.identifier;
+ORDER BY accession.identifier;
