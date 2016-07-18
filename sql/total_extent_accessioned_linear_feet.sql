188ddad0a5507084b7e67c24f8e4e9e8d3f670d0
Noah Huffman <noah.huffman@gmail.com>
2016-07-18 10:23:39 -0400

    break out by UA vs. non-UA

--- sql/total_extent_accessioned_linear_feet.sql
+++ sql/total_extent_accessioned_linear_feet.sql
@@ -1,6 +1,24 @@
 SELECT 
     COUNT(*) AS 'Total accessions measured in linear feet',
-    ROUND(SUM(extent.number), 2) AS 'Linear feet'
+    COUNT(CASE
+        WHEN accession.identifier LIKE '%ua%' THEN 1
+        ELSE NULL
+    END) AS Count_UAAccessions,
+    ROUND(SUM(CASE
+                WHEN accession.identifier LIKE '%ua%' THEN extent.number
+                ELSE 0
+            END),
+            2) AS 'UA Linear feet',
+    COUNT(CASE
+        WHEN accession.identifier NOT LIKE '%ua%' THEN 1
+        ELSE NULL
+    END) AS Count_OtherAccessions,
+    ROUND(SUM(CASE
+                WHEN accession.identifier NOT LIKE '%ua%' THEN extent.number
+                ELSE 0
+            END),
+            2) AS 'Other Linear feet',
+    ROUND(SUM(extent.number), 2) AS 'Total Linear feet'
 FROM
     accession
         LEFT JOIN
