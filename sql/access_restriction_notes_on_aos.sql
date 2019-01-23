7ba66cbe2c35cc49d336611addd3ff94c9b9b2fa
Noah Huffman <noah.huffman@gmail.com>
2019-01-23 12:07:39 -0500

    Update access_restriction_notes_on_aos.sql
    add search criteria, links to AOs in output

--- sql/access_restriction_notes_on_aos.sql
+++ sql/access_restriction_notes_on_aos.sql
@@ -3,9 +3,12 @@ SELECT
 	resource.title as collection_title,
 	resource.ead_id,
 	note.archival_object_id,
+	/*CREATE url to link directly to AO*/
+	CONCAT('https://archivesspace-staff.lib.duke.edu/resources/',resource.id,'/edit#::archival_object_',archival_object.id) as link_to_ao,
 	archival_object.display_string as archival_object_title,
 	archival_object.ref_id,
 	enumeration_value.value as level,
+	/*output full JSON Blob*/
 	CONVERT(note.notes USING utf8) as note_content,
 	note.id as note_identifier
 
@@ -19,7 +22,9 @@ FROM
 	enumeration_value on archival_object.level_id = enumeration_value.id
 
 WHERE 
-	notes LIKE "%accessrestrict%" 
+	note.notes LIKE "%accessrestrict%" AND
+	/*Include keywords for searching note text below (e.g. dates for expiring restrictions, etc)*/
+	note.notes LIKE "% 2020%"
 AND
 	archival_object_id IS NOT NULL
 ORDER BY resource.identifier
