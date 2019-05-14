7e62712d58c82ac829bf2b3fd4267b380ec12fc0
Noah Huffman <noah.huffman@gmail.com>
2019-05-14 15:32:46 -0400

    add-more-bad-html
    Include meta/title tags so page title doesn't randomly come from some random EAD <title> element.

--- python/new-and-updated-eads-report-html.py
+++ python/new-and-updated-eads-report-html.py
@@ -47,7 +47,7 @@ destination = raw_input('Save report to: ')
 f = open(destination+'new-ead-report'+today_date+'.html', 'w')
 
 #start writing out HTML file
-f.write("<html><head><h1>Finding Aid Report</h1></head><body><p><a href=\"#new\">New Finding Aids</a> | <a href=\"#updated\">Updated Finding Aids</a> | <a href=\"#counts\">Counts</a></p><h2><a name=\"new\"></a>New Finding Aids Posted {0} to {1}</h2>".format(quarter_start_date, quarter_end_date))
+f.write("<html><head><meta><title>Finding Aid Report</title></meta><h1><title>Finding Aid Report</title></h1></head><body><p><a href=\"#new\">New Finding Aids</a> | <a href=\"#updated\">Updated Finding Aids</a> | <a href=\"#counts\">Counts</a></p><h2><a name=\"new\"></a>New Finding Aids Posted {0} to {1}</h2>".format(quarter_start_date, quarter_end_date))
 
 with open(new_eads_list,'rb') as csvfile:
 	reader = csv.reader(csvfile)
