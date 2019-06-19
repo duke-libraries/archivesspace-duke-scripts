0889029944063bf9bb5b63ec902af3c2a90538bb
Noah Huffman <noah.huffman@gmail.com>
2019-06-19 15:32:47 -0400

    update-accession-stats-sql

--- sql/accessions_acquired_processing_status.sql
+++ sql/accessions_acquired_processing_status.sql
@@ -1,14 +1,23 @@
-SELECT 
+/* Will only report accession counts for those accessions that have extent recorded in linear feet
+need to run QC scripts first to find accessions without lf count */
+SELECT
     ev1.value AS 'processing_status',
-    COUNT(accession.id) AS '# of accessions'
+    COUNT(accession.id) AS '# of accessions',
+    ROUND(SUM(extent.number), 2) AS 'total_extent_lf'
 FROM
     accession
         LEFT JOIN
     collection_management ON accession.id = collection_management.accession_id
         LEFT JOIN
     enumeration_value ev1 ON collection_management.processing_status_id = ev1.id
+        LEFT JOIN
+    extent on accession.id = extent.accession_id
+        LEFT JOIN
+    enumeration_value ev2 ON extent.extent_type_id = ev2.id
 WHERE
     repo_id = 2
-        AND accession.accession_date >= '20150701'
-        AND accession.accession_date <= '20160631'
-GROUP BY ev1.value;
+        AND accession.accession_date >= '20180701'
+        AND accession.accession_date <= '20190631'
+        AND ev2.value LIKE '%linear%'
+GROUP BY
+    ev1.value WITH ROLLUP
