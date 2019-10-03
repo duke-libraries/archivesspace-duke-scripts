2971e39702a85023e643a70dbd2e1e254672370c
Noah Huffman <noah.huffman@gmail.com>
2019-10-03 16:19:20 -0400

    overhaul to use xml only and python3

--- python/new-and-updated-eads-report-html.py
+++ python/new-and-updated-eads-report-html.py
@@ -1,127 +1,110 @@
-import sys, os, glob
-import requests
-import json
-import re
-import ConfigParser
-import time
+import os, glob
+from lxml import etree as ET
 import datetime
 import csv
 
-#Python 2.7
+#Python 3+
 
-# Script generates a report of new and updated finding aids (and counts) since the date specified using a very convoluted process that only makes sense at Duke
-# Script takes as input a .txt file with a list of new EADIDs, the path to published EADs at Duke,the beginning date of the quarter, and a destination path to save the report.
+# Script generates an HTML report of new and updated finding aids (and counts) based on dates specified at runtime.
+# Script takes as input a .txt file with a list of new EADIDs, the path to published EADs at Duke,the beginning and end dates of reporting period, and a destination path to save the report.
+# This probably only makes sense in Duke context
 
-# local config file, contains variables
-configFilePath = 'local_settings.cfg'
-config = ConfigParser.ConfigParser()
-config.read(configFilePath)
-
-# URL parameters dictionary, used to manage common URL patterns
-dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'repository':config.get('ArchivesSpace', 'repository'), 'user': config.get('ArchivesSpace', 'user'), 'password': config.get('ArchivesSpace', 'password')}
-baseURL = '{baseURL}'.format(**dictionary)
-repositoryBaseURL = '{baseURL}/repositories/{repository}/'.format(**dictionary)
-
-new_eads_list = raw_input('Path to EADID List CSV: ')
-
-current_eads_path = raw_input('Path to published EADs: ')
-
-# authenticates the session
-auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=false'.format(**dictionary)).json()
-session = auth["session"]
-
-#if auth.status_code == 200:
-print "Authenticated!"
-headers = {'X-ArchivesSpace-Session':session}
-
-#set EAD export options: number components and include DAOs
-export_options = '?include_daos=true&numbered_cs=true&include_unpublished=false'
+new_eads_list = input('Path to EADID List CSV: ')
+current_eads_path = input('Path to published EADs: ')
+os.chdir(current_eads_path)
 
 #today's date
 today_date = datetime.datetime.today().strftime('%Y-%m-%d')
 
-quarter_start_date = raw_input("Quarter Start Date (YYYY-MM-DD): ")
-quarter_end_date = raw_input("Quarter End Date (YYYY-MM-DD): ")
+quarter_start_date = input("Quarter Start Date (YYYY-MM-DD): ")
+quarter_end_date = input("Quarter End Date (YYYY-MM-DD): ")
 
 #location to store text file of report
-destination = raw_input('Save report to: ')
-f = open(destination+'new-ead-report'+today_date+'.html', 'w')
+destination = input('Save report to: ')
+
+f = open(destination+'new-ead-report'+today_date+'.html', 'w', encoding="utf-8")
+
+with open(new_eads_list,'rt') as csvfile:
+    reader = csv.reader(csvfile)
+    new_eadid_list = []
+    for row in reader:
+        eadid = row[0].strip()
+        new_eadid_list.append(eadid)
+    print (str(len(new_eadid_list)) + " input EADs in file: " + str(new_eadid_list))
+
+
+#Initialize some variables
+#Create two file lists for new eads and updated eads and running counts
+new_files = []
+updated_files = []
+all_eadid_list = []
+
+#Read over EAD directory and get files modified after specified date.
+for file in sorted(glob.glob("*.xml")):
+    modified_time = os.path.getmtime(file)
+    modified_time_iso = datetime.datetime.fromtimestamp(modified_time).strftime('%Y-%m-%d')
+    eadid = file.replace(".xml","")
+    all_eadid_list.append(eadid)
+    
+
+    if eadid in new_eadid_list:
+        new_files.append(file)
+        
+    if eadid not in new_eadid_list:
+        #only get subset of files modified during quarter (as specified at runtime)
+        if quarter_start_date <= modified_time_iso <= quarter_end_date:
+            updated_files.append(file)
+            #print ("Updated EAD: " + eadid + " | " + modified_time_iso)
+
+new_files_count = str(len(new_files))
+updated_files_count = str(len(updated_files))
+
+print (new_files_count + " new EADs in directory matching input: " + str(new_files))
+print (updated_files_count + " updated EADs in directory: " + str(updated_files))
+
+for item in new_eadid_list:
+    if item not in all_eadid_list:
+        print("WARNING: EADID/Filename Mismatch for: " + item)
+        
+#Write a new section for updated EADs
+print ("writing new/updated finding aids to HTML file...")
 
 #start writing out HTML file
-f.write("<html><head><meta><title>Finding Aid Report</title></meta><h1><title>Finding Aid Report</title></h1></head><body><p><a href=\"#new\">New Finding Aids</a> | <a href=\"#updated\">Updated Finding Aids</a> | <a href=\"#counts\">Counts</a></p><h2><a name=\"new\"></a>New Finding Aids Posted {0} to {1}</h2>".format(quarter_start_date, quarter_end_date))
+f.write("<html><head><meta><title>Finding Aid Report</title></meta></head><body><p><a href=\"#new\">{2} New Finding Aids</a> <br> <a href=\"#updated\">{3} Updated Finding Aids</a> </p> <h2><a name=\"new\"></a>New Finding Aids ({0} to {1})</h2>".format(quarter_start_date, quarter_end_date,new_files_count, updated_files_count))
 
-with open(new_eads_list,'rb') as csvfile:
-	reader = csv.reader(csvfile)
-	#next(csvfile, None) #ignore header row
-	print "writing new finding aids to HTML file..."
-	new_count = 0
-	eadid_list = []
-	for row in reader:
-		new_count = new_count + 1
-		eadid = row[0]
+for file in new_files:
+    xml_doc = ET.parse(file)
+    root = xml_doc.getroot()
+    namespace = "{urn:isbn:1-931666-22-9}"
+    finding_aid_title = root.find(".//{0}archdesc/{0}did/{0}unittitle".format(namespace)).text
+    finding_aid_dates = root.find(".//{0}archdesc/{0}did/{0}unitdate[1]".format(namespace)).text
+    extent = root.find(".//{0}archdesc/{0}did/{0}physdesc/{0}extent[1]".format(namespace)).text
+    eadid = root.find(".//{0}eadheader/{0}eadid".format(namespace))
+    ead_location = eadid.get("url")
+    abstract = root.find(".//{0}archdesc/{0}did/{0}abstract".format(namespace)).text
+    
+    #write a <div> for every new EAD
+    f.write("<div><h3><a href=\"{3}\">{0}, {1}</a> ({1})</h3><p>{4}</p><hr></div>".format(finding_aid_title, finding_aid_dates, extent, ead_location, abstract))
 
-		eadid_list.append(eadid)
-#advanced search for EADID
-		results = requests.get(repositoryBaseURL + '/search?page=1&aq={\"query\":{\"field\":\"ead_id\",\"value\":\"'+eadid+'\",\"jsonmodel_type\":\"field_query\",\"negated\":false,\"literal\":false}}', headers=headers).json()
+#Updated Finding Aid Section Header
+f.write("<h2><a name=\"updated\"></a>Updated Finding Aids ({0} to {1})</h2>".format(quarter_start_date, quarter_end_date))
 
-#Make sure the EADID input matches an EADID value in ASpace
-		if results["total_hits"] is not 0:
-			#get the URI of the first search result (should only be one)
-			uri = results["results"][0]["id"]
-			#get JSON for the resource based on above URI
-			resource_json = (requests.get(baseURL + uri, headers=headers)).json()
-			#print resource_json
-			#get URI of Resource record
-			resource_uri = resource_json["uri"]
+for file in updated_files:
+    xml_doc = ET.parse(file)
+    root = xml_doc.getroot()
+    namespace = "{urn:isbn:1-931666-22-9}"
+    finding_aid_title = root.find(".//{0}archdesc/{0}did/{0}unittitle".format(namespace)).text
+    finding_aid_dates = root.find(".//{0}archdesc/{0}did/{0}unitdate[1]".format(namespace)).text
+    extent = root.find(".//{0}archdesc/{0}did/{0}physdesc/{0}extent[1]".format(namespace)).text
+    eadid = root.find(".//{0}eadheader/{0}eadid".format(namespace))
+    ead_location = eadid.get("url")
+    #abstract = root.find(".//{0}archdesc/{0}did/{0}abstract".format(namespace)).text
+    modified_time = os.path.getmtime(file)
+    modified_time_iso = datetime.datetime.fromtimestamp(modified_time).strftime('%Y-%m-%d')
+       
+    #write a <div> for every updated EAD, just include titles, links, and modified dates
+    f.write("<div><p><a href=\"{2}\">{0}, {1}</a> (updated: {3})</p></div>".format(finding_aid_title, finding_aid_dates, ead_location, modified_time_iso))
 
-#stuff for report
-			finding_aid_title = resource_json["finding_aid_title"].replace("\n","").encode('utf8')
-			extent_number = resource_json["extents"][0]["number"]
-			extent_type = resource_json["extents"][0]["extent_type"]
-			#get finding aid link
-			ead_location = resource_json["ead_location"]
-
-			notes = resource_json["notes"]
-			for note in notes:
-				if note["type"] == "abstract":
-						abstract = note["content"][0].encode('utf8')
-			#write a <div> for every new EAD
-			f.write("<div><h3><a href=\"{3}\">{0}</a> ({1} {2})</h3><p>{4}</p><hr></div>".format(finding_aid_title, extent_number, extent_type, ead_location, abstract))
-
-#Write a new section for updated EADs
-f.write("<h2><a name=\"updated\"></a>Finding Aids Updated {0} to {1}</h2>".format(quarter_start_date, quarter_end_date))
-
-#Read over EAD directory and get files modified after specified date. Then, get finding aid URLs out of ASpace
-
-for root, dirs, filenames in os.walk(current_eads_path):
-	print "writing updated finding aids to HTML file..."
-	update_count = 0
-	for filename in sorted(filenames):
-		filepath = os.path.join(root, filename)
-		eadid = filename.replace(".xml","")
-		modified_time = os.path.getmtime(filepath)
-		modified_time_iso = datetime.datetime.fromtimestamp(modified_time).strftime('%Y-%m-%d')
-		#print eadid + " | " + modified_time_iso
-
-		#exclude New finding aids
-		if eadid not in eadid_list:
-
-			#only get records modified during quarter (as specified at runtime)
-			if quarter_start_date <= modified_time_iso <= quarter_end_date:
-				update_count = update_count + 1
-				print eadid + " | " + modified_time_iso
-				results = (requests.get(repositoryBaseURL + '/search?page=1&aq={\"query\":{\"field\":\"ead_id\",\"value\":\"'+eadid+'\",\"jsonmodel_type\":\"field_query\",\"negated\":false,\"literal\":false}}', headers=headers)).json()
-				if results["total_hits"] is not 0:
-					#get the URI of the first search result (should only be one)
-					uri = results["results"][0]["id"]
-					resource_json = (requests.get(baseURL + uri, headers=headers)).json()
-					ead_location = resource_json["ead_location"]
-					finding_aid_title = resource_json["finding_aid_title"].replace("\n","").encode('utf8')
-					#write a <div> for every updated EAD, just include titles, links, and modified dates
-					f.write("<div><p><a href=\"{1}\">{0}</a> (updated: {2})</p></div>".format(finding_aid_title, ead_location, modified_time_iso))
-
-#Write out some stats at the bottom
-f.write("<p><a name=\"counts\">TOTAL NEW: {0}<br/>TOTAL UPDATED: {1}</a></p></body></html>".format(new_count, update_count))
+f.write("</body></html>")
 f.close
-
-print "All Done!!"
+print ("All Done!!")
