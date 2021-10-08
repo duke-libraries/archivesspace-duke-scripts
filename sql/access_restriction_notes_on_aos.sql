147d2e78a311e396cfd6a6a3768ac721a290b85c
Noah Huffman <noah.huffman@gmail.com>
2021-10-08 16:04:22 -0400

    Update access_restriction_notes_on_aos.sql
    include note_persistent_id

--- sql/access_restriction_notes_on_aos.sql
+++ sql/access_restriction_notes_on_aos.sql
@@ -1,16 +1,17 @@
 SELECT 
-	resource.identifier as collection_id,
+	archival_object.repo_id as repo_id,
+    resource.id as resource_id,
 	resource.title as collection_title,
-	resource.ead_id,
-	note.archival_object_id,
+	resource.ead_id as ead_id,
+	note.archival_object_id as ao_id,
 	/*CREATE url to link directly to AO*/
 	CONCAT('https://archivesspace-staff.lib.duke.edu/resources/',resource.id,'/edit#tree::archival_object_',archival_object.id) as link_to_ao,
 	archival_object.display_string as archival_object_title,
-	archival_object.ref_id,
-	enumeration_value.value as level,
+	archival_object.ref_id as ao_ref_id,
+	enumeration_value.value as ao_level,
 	/*output full JSON Blob*/
-	CONVERT(note.notes using 'utf8') as note_content,
-	note.id as note_identifier
+	REPLACE(REPLACE(CONVERT(note.notes using 'utf8'),CHAR(13),''), CHAR(10), '') as note_content,
+	note_persistent_id.persistent_id as note_persistent_id
 
 FROM 
 	note
@@ -20,11 +21,13 @@ FROM
 	resource on archival_object.root_record_id = resource.id
 		LEFT JOIN
 	enumeration_value on archival_object.level_id = enumeration_value.id
+		LEFT JOIN
+	note_persistent_id on note.id = note_persistent_id.note_id
 
 WHERE 
 	note.notes LIKE "%accessrestrict%" AND
 	/*Include keywords for searching note text below (e.g. dates for expiring restrictions, etc)*/
-	note.notes LIKE "% 2020%"
+	(note.notes LIKE "% 2020%" OR note.notes LIKE "% 2021%")
 AND
 	archival_object_id IS NOT NULL
 ORDER BY resource.identifier
