3d6019ca0d53da6131c9e1037b38d252f92ca088
engartst <engartst@gmail.com>
2024-02-21 16:20:05 -0500

    added session auth

--- python/as_delete_do.py
+++ python/as_delete_do.py
@@ -10,6 +10,8 @@ import sys
 import configparser
 import logging
 
+DEBUG = True
+
 # CONSTANTS
 CSV_FILE = 'DOs.csv'
 CONFIG_FILE = 'asnake_local_settings.cfg'
@@ -30,6 +32,25 @@ logging.info(f'AS_api_url: {AS_api_url}')
 logging.info(f'AS_repository_id: {AS_repository_id}')
 logging.info(f'CSV_FILE: {CSV_FILE}')
 
+# Session Authentication
+auth = requests.post(AS_api_url + '/users/' + AS_username+ '/login?password='
+                     + AS_password)
+if auth.status_code == 200:
+    auth_json = auth.json()
+    if DEBUG:
+        print("DEBUG======================DEBUG")
+        print(f'Response text: \n {auth_json}')
+        print("DEBUG======================DEBUG")
+    print('Login successful! Continuing process.')
+else:
+    print(f'Login failed! Response text: \n {auth.text}')
+    print('Exiting process.')
+    exit()
+
+session = auth_json['session']
+headers = {'X-ArchivesSpace-Session': session,
+           'Content_Type': 'application/json'}
+
 with open(CSV_FILE, 'r') as f:
     reader = csv.reader(f)
     DOs = list(reader)
@@ -40,5 +61,5 @@ for i in DOs:
     print(f'URL: {AS_api_url}/repositories/{AS_repository_id}/digital_objects/{i[0]}')
     logging.info(f'URL: {AS_api_url}/repositories/{AS_repository_id}/digital_objects/{i[0]}')
     # Delete the # in the next line to actually delete the DOs
-    response = requests.delete(f'{AS_api_url}/repositories/{AS_repository_id}/digital_objects/{i[0]}', auth=(AS_username, AS_password))
+    response = requests.delete(f'{AS_api_url}/repositories/{AS_repository_id}/digital_objects/{i[0]}', headers = headers, auth=(AS_username, AS_password))
     logging.info(f'Response: {response.status_code} - {response.text}')
